a7db92f50e94edeea19a1d5aebe6685a3608fab4
Kieran Winfield <kieran.winfield464@gmail.com>
2019-03-04 10:55:29 +0000

    Improved comment for the intialize function

--- Auto deposit Materials/AutoDepositMaterials.lua
+++ Auto deposit Materials/AutoDepositMaterials.lua
@@ -4,11 +4,15 @@ ADM = {}
 -- Easier to display the name of the addon if needed
 ADM.name = "Auto Deposit Materials"
 
--- Intialize the addon
+-- Intialize the addon and test to make sure the addon has loaded
 function ADM:Intialize()
     d("Addon is working")
 end
 
+-- Drop materials into bank
+function ADM.Bank()
+
+end
 
 -- Register event handlers
 EVENT_MANAGER:RegisterForEvent(ADM.name, EVENT_OPEN_BANK, ADM.Intialize)
